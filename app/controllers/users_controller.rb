d3cbe18baf8fd0c13e6380cb2301ef7a06db5dbf
Ko-s-y <song.1209@icloud.com>
2022-06-19 00:05:30 +0900

    userテーブルにimageカラムの追加, 簡易的なプロフィールページ実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,22 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+
   def show
   end
+
+  def update
+    if current_user.update(current_user_params)
+      flash[:notice] = "アカウント情報を更新しました。"
+    else
+      flash[:alert] = "アカウント情報の更新に失敗しました。"
+    end
+    redirect_to users_show_path
+  end
+
+  private
+
+  def current_user_params
+    params.require(:user).permit(:avatar)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   devise_for :users
-  get 'users/show'
+
+  get 'users/show', to: 'users#show'
+  post 'users/show.:id', to: 'users#update'
   get 'pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
