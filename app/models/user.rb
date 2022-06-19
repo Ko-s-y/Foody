dbccd8f28dab38db31f16c42701e7baed2af4bda
Ko-s-y <song.1209@icloud.com>
2022-06-19 13:45:16 +0900

    css修正とアカウント名を英数字のみにvalidate

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,:recoverable, :rememberable, :validatable
   devise :validatable, password_length: 8..128
-  validates :name, presence: true, uniqueness: { scope: :name }, length: { maximum: 12, minimum: 6 }
+
+  with_options presence: true, uniqueness: { scope: :name }, length: { maximum: 12, minimum: 6 }, format: { with: /\A[a-zA-Z0-9]+\z/, message: 'は半角英数字のみ使用してください。' } do
+    validates :name
+  end
+
 end
