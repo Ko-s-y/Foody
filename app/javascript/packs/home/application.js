02aebd6619b2ba9818b633c6f9fa5f4dadbbfec1
Ko-s-y <song.1209@icloud.com>
2022-06-22 23:55:50 +0900

    homeページにて最初に1度リロードするように修正

--- app/javascript/packs/home/application.js
+++ app/javascript/packs/home/application.js
@@ -60,5 +60,16 @@ $(function () {
     };
   });
 
+// 連続でhomeへのリンクを押すとタイマーが多重動作してslide-imageの表示が狂うため
+  $(document).ready(function() {
+    if (window.name != "reloaded") {
+      alert('リロードします')
+      location.reload();
+      window.name = "reloaded";
+    } else {
+      window.name = "";
+    }
+  });
+
   startTimer();
 });
