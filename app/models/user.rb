c3ee51e6601d88ecb8227e1a29ece86acc1f0096
Ko-s-y <song.1209@icloud.com>
2022-06-22 19:17:46 +0900

    rubocop違反の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   devise :validatable, password_length: 8..128
 
-  with_options presence: true, uniqueness: { scope: :name }, length: { maximum: 12, minimum: 6 }, format: { with: /\A[a-zA-Z0-9]+\z/, message: 'は半角英数字のみ使用してください。' } do
+  with_options presence: true,
+               uniqueness: { scope: :name },
+               length: { maximum: 12, minimum: 6 },
+               format: { with: /\A[a-zA-Z0-9]+\z/, message: 'は半角英数字のみ使用してください。' } do
     validates :name
   end
 
