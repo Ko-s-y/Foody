ba7f9b270001524aa3a402f419667fb819490278
Ko-s-y <song.1209@icloud.com>
2022-06-29 23:42:46 +0900

    コメントにvalidate追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,21 +5,23 @@ class CommentsController < ApplicationController
     @comment = current_user.comments.new(comment_params)
     if @comment.save
       flash[:notice] = "投稿にコメントしました"
-      redirect_to post_path(@comment.post_id)
     else
-      flash.now[:alert] = "コメントに失敗しました"
-      render "show"
+      flash[:alert] = "コメントの投稿に失敗しました"
     end
+    redirect_to post_path(@comment.post_id)
   end
 
   def destroy
-    @post = Post.find(params[:post_id])
-    comment = @post.comments.find(params[:id])
-    comment.destroy
-    flash[:notice] = "コメントを削除しました"
-    redirect_to post_path(@post.id)
+    post = Post.find(params[:post_id])
+    comment = post.comments.find(params[:id])
+    if current_user.id == comment.user.id
+      comment.destroy
+      flash[:notice] = "コメントを削除しました"
+    else
+      flash[:alert] = "エラーが発生しました"
+    end
+    redirect_to post_path(post.id)
   end
-
 
   private
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   has_one_attached :avatar
+
+  validates :comment_content, presence: true, length: { maximum: 100 }
 end
