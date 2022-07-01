8d4e5b7bc4dd70a682a782e702ff27d5958ea07f
Ko-s-y <song.1209@icloud.com>
2022-07-01 18:01:53 +0900

    アイコン画像の変更のform_forをform_withに改善

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'users/profile/:id', to: 'users#profile' # 他ユーザーの詳細画面
-  get 'users/show', to: 'users#show' # 自分の詳細画面
-  post 'users/show.:id', to: 'users#update' # 自分のアイコン変更
+  get 'users/show' # 自分の詳細画面
+  post 'users/show', to: 'users#update' # 自分のアイコン変更
 
   get 'pages/home'
   get 'pages/about'
