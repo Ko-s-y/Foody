5b6035e683717f901b7938a460a17d33e539f440
Ko-s-y <song.1209@icloud.com>
2022-07-06 01:04:36 +0900

    投稿に対するいいね、rememberをもらった数を表示する機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,17 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
+# my page
   def show
     @posts = current_user.posts.all.order(id: :desc)
+    received_counter(@posts)
   end
 
+# 他ユーザーページ
   def profile
     @user = User.find_by(name: params[:name])
     @posts = @user.posts.all.order(id: :desc)
+    received_counter(@posts)
   end
 
   def update
@@ -23,5 +27,18 @@ class UsersController < ApplicationController
 
   def current_user_params
     params.require(:user).permit(:avatar)
+  end
+
+  def received_counter(posts)
+    # いいねをもらった回数
+    @received_like = 0
+    @posts.each do |post|
+      @received_like += post.likes.count
+    end
+    # rememberされた回数
+    @received_remember = 0
+    @posts.each do |post|
+      @received_remember += post.remembers.count
+    end
   end
 end
