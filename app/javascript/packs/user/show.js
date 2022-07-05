8a34aea7a59aadab5babc092c1a5e1b2b79ef71b
Ko-s-y <song.1209@icloud.com>
2022-07-05 20:41:37 +0900

    my pageにてfoodyでの活動を確認する機能を追加

--- app/javascript/packs/user/show.js
+++ app/javascript/packs/user/show.js
@@ -1,28 +1,10 @@
-// 投稿セクション出現関数
+// 投稿一覧セクション出現関数
 $(function() {
   $(document).on('click', '.appear-posted-section', function() {
-    $('.liked-posts-section').css({
-      'display': 'none'
-    });
     $('.remember-posts-section').css({
       'display': 'none'
     });
     $('.user-posted-section').css({
-      'display': 'block'
-    });
-  });
-});
-
-//　　いいねした投稿セクション出現関数
-$(function() {
-  $(document).on('click', '.appear-liked-section', function() {
-    $('.remember-posts-section').css({
-      'display': 'none'
-    });
-    $('.user-posted-section').css({
-      'display': 'none'
-    });
-    $('.liked-posts-section').css({
       'display': 'block'
     });
   });
@@ -31,9 +13,6 @@ $(function() {
 // rememberした投稿セクション出現関数
 $(function() {
   $(document).on('click', '.appear-remember-section', function() {
-    $('.liked-posts-section').css({
-      'display': 'none'
-    });
     $('.user-posted-section').css({
       'display': 'none'
     });
@@ -43,3 +22,32 @@ $(function() {
   });
 });
 
+// userの活動実績表示関数
+$(function() {
+  $(document).on('click', '.appear-user-data', function() {
+    $('.appear-user-data').css({
+      'display': 'none'
+    });
+    $('.disappear-user-data').css({
+      'display': 'block'
+    });
+    $('.user-data').css({
+      'display': 'block'
+    });
+  });
+});
+
+// userの活動実績の非表示関数
+$(function() {
+  $(document).on('click', '.disappear-user-data', function() {
+    $('.disappear-user-data').css({
+      'display': 'none'
+    });
+    $('.appear-user-data').css({
+      'display': 'block'
+    });
+    $('.user-data').css({
+      'display': 'none'
+    });
+  });
+});
