b1a5114f82a622af11fdcf8e0acee12b93e68633
Ko-s-y <song.1209@icloud.com>
2022-07-09 11:43:49 +0900

    notificationsのコメント内容にcss適用

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,7 +4,7 @@ class NotificationsController < ApplicationController
   def index
     @notifications = current_user.passive_notifications.order(created_at: :desc)
     @notifications = @notifications.where.not(visitor_id: current_user.id)
-    # notifications訪れると既読になる仕様
+    # notifications訪れるとその時表示されたnotificationsは全て既読になる仕様
     @notifications.where(checked: false).each do |notification|
       notification.update(checked: true)
     end
