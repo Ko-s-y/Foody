6778446b88335f98e253c237c9b3e2a1e299c078
Ko-s-y <song.1209@icloud.com>
2022-07-09 15:29:15 +0900

    1度閲覧したinfoはリロード再び表示されない機能実装

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,9 +4,10 @@ class NotificationsController < ApplicationController
   def index
     @notifications = current_user.passive_notifications.order(created_at: :desc)
     @notifications = @notifications.where.not(visitor_id: current_user.id)
+    @notifications = @notifications.where(checked: false)
     # notifications訪れるとその時表示されたnotificationsは全て既読になる仕様
-    @notifications.where(checked: false).each do |notification|
-      notification.update(checked: true)
-    end
+    # @notifications.where(checked: false).each do |notification|
+    #   notification.update(checked: true)
+    # end
   end
 end
