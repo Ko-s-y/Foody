74ce50b03843019e57bcd25f10c9073bca5ddaa9
Ko-s-y <song.1209@icloud.com>
2022-07-10 23:38:00 +0900

    infoページの通知がない場合のレイアウト変更,link追加

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -5,9 +5,5 @@ class NotificationsController < ApplicationController
     @notifications = current_user.passive_notifications.order(created_at: :desc)
     @notifications = @notifications.where.not(visitor_id: current_user.id)
     @notifications = @notifications.where(checked: false)
-    # notifications訪れるとその時表示されたnotificationsは全て既読になる仕様
-    # @notifications.where(checked: false).each do |notification|
-    #   notification.update(checked: true)
-    # end
   end
 end
