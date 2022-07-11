7978d4b4b3b6297c5b39eded994bb061145354ff
Ko-s-y <song.1209@icloud.com>
2022-07-11 13:21:18 +0900

    view側で行っていた既読のデータ更新をcontrollerに移動

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,9 +1,18 @@
 class NotificationsController < ApplicationController
   before_action :authenticate_user!
+  after_action :checked_true!, only: :index
 
   def index
     @notifications = current_user.passive_notifications.order(created_at: :desc)
     @notifications = @notifications.where.not(visitor_id: current_user.id)
     @notifications = @notifications.where(checked: false)
+  end
+
+  private
+
+  def checked_true!
+    @notifications.each do |notification|
+      notification.update(checked: true)
+    end
   end
 end
