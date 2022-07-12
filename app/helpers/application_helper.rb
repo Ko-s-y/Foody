057db5980d5008bc078023a1dd7ae7a9ae55bfc7
Ko-s-y <song.1209@icloud.com>
2022-07-12 17:49:15 +0900

    headerに新規通知があれば数を表示する機能追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,4 +26,8 @@ module ApplicationHelper
       ActionController::Base.helpers.asset_path('tortelli.jpg')
     end
   end
+
+  def new_notifications
+    current_user.passive_notifications.where(checked: false)
+  end
 end
