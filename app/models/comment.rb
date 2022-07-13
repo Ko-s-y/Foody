9d35d6126c0695c881c12c1ba28c96c0cbc1d600
Ko-s-y <song.1209@icloud.com>
2022-07-13 19:57:50 +0900

    commentモデルにも2つのキーワード検索の機能を追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -8,6 +8,16 @@ class Comment < ApplicationRecord
   validates :comment_content, presence: true, length: { maximum: 200 }
 
   def self.looks(search, word)
-    @comment = Comment.where("comment_content LIKE?","%#{word}%")
+    # 空文字があれば
+    if word.include?(" ") || word.include?("　")
+      words = word.split(/[[:blank:]]+/)
+      word_1 = words.first
+      word_2 = words.second
+      @comments = Comment.where("comment_content LIKE ? AND comment_content LIKE ?", "%#{word_1}%", "%#{word_2}%")
+    # 空文字なければ
+    else
+      @comments = Comment.where("comment_content LIKE ?", "%#{word}%")
+    end
+    @comments = @comments.order(id: :DESC)
   end
 end
