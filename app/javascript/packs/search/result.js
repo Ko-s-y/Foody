5bf0a243769e28c55f56ba75a6aa57ee287ae747
Ko-s-y <song.1209@icloud.com>
2022-07-14 15:04:27 +0900

    検索のヒントの表示/非表示ボタン追加

---
+++ app/javascript/packs/search/result.js
@@ -0,0 +1,23 @@
+// 検索ヒント表示関数
+$(function() {
+  $(document).on('click', '.search-advise-btn', function() {
+    $('.search-result').css({
+      'display': 'none'
+    });
+    $('.search-advise').css({
+      'display': 'block'
+    });
+  });
+});
+
+// 検索ヒント非表示関数
+$(function() {
+  $(document).on('click', '.dis-advise-btn', function() {
+    $('.search-advise').css({
+      'display': 'none'
+    });
+    $('.search-result').css({
+      'display': 'block'
+    });
+  });
+});
