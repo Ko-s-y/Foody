9289f7c4951a43fcd8445d1ee047d9e1d41c4fa4
Ko-s-y <song.1209@icloud.com>
2022-07-15 14:57:55 +0900

    herokuエラー解消

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,6 @@ class ContactMailer < ApplicationMailer
 
   def contact_mail(contact, user)
     @contact = contact
-    mail
-      from: '"Foody運営" <noreply@yoursite.com>', to: user.email, bcc: ENV["FOODY_PRODUCTION_USER"], bcc: ENV["FOODY_ADMIN_ADDRESS"], bcc: ENV["ACTION_MAILER_USER"], bcc: ENV["USER_ADDRESS"], subject: "お問い合わせについて【自動送信】"
+    mail from: '"Foody運営" <noreply@yoursite.com>', to: user.email, bcc: ENV["ACTION_MAILER_USER"], bcc: ENV["USER_ADDRESS"], subject: "お問い合わせについて【自動送信】"
   end
 end
