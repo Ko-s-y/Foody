d4e8406ca8ef9619ad1593a91cc1141869d8f6ec
Ko-s-y <song.1209@icloud.com>
2022-07-15 18:56:53 +0900

    メール送信するbcc追加

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,13 @@
 class ContactMailer < ApplicationMailer
-  # ENV["FOODY_ADMIN_ADDRESS"]は本番環境用のadminのheroku環境変数のicloudアドレス
-  # ENV["ACTION_MAILER_USER"]は開発環境用の.env環境変数のgmailアドレス
+  # ENV["FOODY_ADMIN_ADDRESS"]は本番環境で使用しているadminのheroku環境変数のicloudアドレス
+  # ENV["FOODY_PRODUCTION_USER"]は本番環境で使用しているadminのheroku環境変数のgmailアドレス
   # ENV["USER_ADDRESS"]は開発環境用の.env環境変数のicloudアドレス
+  # ENV["ACTION_MAILER_USER"]は開発環境用の.env環境変数のgmailアドレス
+
+  # mail from: '"Foody運営" <noreply@yoursite.com>', to: user.email, bcc: ENV["FOODY_ADMIN_ADDRESS"], bcc: ENV["ACTION_MAILER_USER"], bcc: ENV["USER_ADDRESS"], subject: "お問い合わせについて【自動送信】"
 
   def contact_mail(contact, user)
     @contact = contact
-    mail from: '"Foody運営" <noreply@yoursite.com>', to: user.email, bcc: ENV["FOODY_ADMIN_ADDRESS"], bcc: ENV["ACTION_MAILER_USER"], bcc: ENV["USER_ADDRESS"], subject: "お問い合わせについて【自動送信】"
+    mail from: '"Foody運営" <noreply@foody.com>', to: user.email, bcc: ENV["FOODY_ADMIN_ADDRESS"], bcc: ENV["FOODY_PRODUCTION_USER"], bcc: ENV["USER_ADDRESS"], bcc: ENV["ACTION_MAILER_USER"], subject: "お問い合わせについて【自動送信】"
   end
 end
