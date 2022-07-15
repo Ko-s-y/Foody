dcb5dbb53b0a869acf6c51d212c6a18ede478feb
Ko-s-y <song.1209@icloud.com>
2022-07-15 14:54:38 +0900

    herokuエラー解消

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,12 +3,6 @@ class ContactMailer < ApplicationMailer
   def contact_mail(contact, user)
     @contact = contact
     mail
-      from: '"Foody運営" <noreply@yoursite.com>',
-      to: user.email,
-      bcc: ENV["FOODY_PRODUCTION_USER"], # 本番環境heroku環境変数,gmailアドレス
-      bcc: ENV["FOODY_ADMIN_ADDRESS"], # 本番環境heroku環境変数,icloudアドレス
-      bcc: ENV["ACTION_MAILER_USER"], # 開発環境用.env変数,gmailアドレス
-      bcc: ENV["USER_ADDRESS"], # 開発環境用.env変数,icloudアドレス
-      subject: "お問い合わせについて【自動送信】"
+      from: '"Foody運営" <noreply@yoursite.com>', to: user.email, bcc: ENV["FOODY_PRODUCTION_USER"], bcc: ENV["FOODY_ADMIN_ADDRESS"], bcc: ENV["ACTION_MAILER_USER"], bcc: ENV["USER_ADDRESS"], subject: "お問い合わせについて【自動送信】"
   end
 end
