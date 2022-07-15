d9a18f7216c43ea6d0756b6cf326d1f55ade86aa
Ko-s-y <song.1209@icloud.com>
2022-07-15 15:19:22 +0900

    本番環境でのgmail受信を削除

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,10 @@
 class ContactMailer < ApplicationMailer
   # ENV["FOODY_ADMIN_ADDRESS"]は本番環境用のadminのheroku環境変数のicloudアドレス
-  # ENV["FOODY_PRODUCTION_USER"]は本番環境用のadminのheroku環境変数のgmailアドレス
   # ENV["ACTION_MAILER_USER"]は開発環境用の.env環境変数のgmailアドレス
   # ENV["USER_ADDRESS"]は開発環境用の.env環境変数のicloudアドレス
 
   def contact_mail(contact, user)
     @contact = contact
-    mail from: '"Foody運営" <noreply@yoursite.com>', to: user.email, bcc: ENV["FOODY_ADMIN_ADDRESS"], bcc: ENV["FOODY_PRODUCTION_USER"], bcc: ENV["ACTION_MAILER_USER"], bcc: ENV["USER_ADDRESS"], subject: "お問い合わせについて【自動送信】"
+    mail from: '"Foody運営" <noreply@yoursite.com>', to: user.email, bcc: ENV["FOODY_ADMIN_ADDRESS"], bcc: ENV["ACTION_MAILER_USER"], bcc: ENV["USER_ADDRESS"], subject: "お問い合わせについて【自動送信】"
   end
 end
