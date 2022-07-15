d80d125ce566dfc0760d986929a56a0b9858bd87
Ko-s-y <song.1209@icloud.com>
2022-07-15 19:28:23 +0900

    宛先修正

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -5,9 +5,10 @@ class ContactMailer < ApplicationMailer
   # ENV["ACTION_MAILER_USER"]は開発環境用の.env環境変数のgmailアドレス
 
   # mail from: '"Foody運営" <noreply@yoursite.com>', to: user.email, bcc: ENV["FOODY_ADMIN_ADDRESS"], bcc: ENV["ACTION_MAILER_USER"], bcc: ENV["USER_ADDRESS"], subject: "お問い合わせについて【自動送信】"
+  # mail from: '"Foody運営" <noreply@foody.com>', to: user.email, bcc: ENV["FOODY_ADMIN_ADDRESS"], bcc: ENV["FOODY_PRODUCTION_USER"], bcc: ENV["USER_ADDRESS"], bcc: ENV["ACTION_MAILER_USER"], subject: "お問い合わせについて【自動送信】"
 
   def contact_mail(contact, user)
     @contact = contact
-    mail from: '"Foody運営" <noreply@foody.com>', to: user.email, bcc: ENV["FOODY_ADMIN_ADDRESS"], bcc: ENV["FOODY_PRODUCTION_USER"], bcc: ENV["USER_ADDRESS"], bcc: ENV["ACTION_MAILER_USER"], subject: "お問い合わせについて【自動送信】"
+    mail from: '"Foody運営" <noreply@foody.com>', to: user.email, bcc: 'so129@me.com', subject: "お問い合わせについて【自動送信】"
   end
 end
