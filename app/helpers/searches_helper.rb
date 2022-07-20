411c487ebb7029b2156e4936d4327a5fd77e0c09
Ko-s-y <song.1209@icloud.com>
2022-07-20 14:07:55 +0900

    検索ワードがカタカナでひらがなの投稿がヒットする場合のハイライト機能

--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -1,7 +1,9 @@
 module SearchesHelper
   def emphasize_keyword(content, keyword)
-    keyword = keyword.split(/[[:blank:]]+/)
-    content_array = content.split(/\R/)
+    # keyword = keyword.tr('ぁ-ん','ァ-ン').split(/[[:blank:]]+/)
+    # content_array = content.tr('ぁ-ん','ァ-ン').split(/\R/)
+    keyword = keyword.tr('ァ-ン','ぁ-ん').split(/[[:blank:]]+/)
+    content_array = content.tr('ァ-ン','ぁ-ん').split(/\R/)
     first_match_line = content_array.find { |e| /#{keyword}/ =~ e }
     trancate_line = if first_match_line.present?
                       truncate(first_match_line, length: 110).delete '#,`,*,\,'
