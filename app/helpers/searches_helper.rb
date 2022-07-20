1303e531a9629f1d4d9b4acd5ebb471dc4b16baa
Ko-s-y <song.1209@icloud.com>
2022-07-20 14:43:13 +0900

    ひらがな=>カタカナに変更

--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -1,9 +1,11 @@
 module SearchesHelper
   def emphasize_keyword(content, keyword)
-    # keyword = keyword.tr('ぁ-ん','ァ-ン').split(/[[:blank:]]+/)
-    # content_array = content.tr('ぁ-ん','ァ-ン').split(/\R/)
-    keyword = keyword.tr('ァ-ン','ぁ-ん').split(/[[:blank:]]+/)
-    content_array = content.tr('ァ-ン','ぁ-ん').split(/\R/)
+    # カタカナ 　=> ひらがなへの変換処理
+    # keyword = keyword.tr('ァ-ン','ぁ-ん').split(/[[:blank:]]+/)
+    # content_array = content.tr('ァ-ン','ぁ-ん').split(/\R/)
+    # ひらがな 　=> カタカナへの変換処理
+    keyword = keyword.tr('ぁ-ん','ァ-ン').split(/[[:blank:]]+/)
+    content_array = content.tr('ぁ-ん','ァ-ン').split(/\R/)
     first_match_line = content_array.find { |e| /#{keyword}/ =~ e }
     trancate_line = if first_match_line.present?
                       truncate(first_match_line, length: 110).delete '#,`,*,\,'
