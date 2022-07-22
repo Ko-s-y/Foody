25708a2e780ce478421fab756aab9243682dcca0
Ko-s-y <song.1209@icloud.com>
2022-07-23 01:29:38 +0900

    likeを非同期通信に改善

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,14 +4,15 @@ class LikesController < ApplicationController
   def create
     @like = current_user.likes.create(post_id: params[:post_id])
     @like.save
-    post = @like.post
-    post.create_notification_like!(current_user)
-    redirect_to post_path(params[:post_id])
+    @post = @like.post
+    @post.create_notification_like!(current_user)
+    # redirect_to post_path(params[:post_id]),ajaxいらなければ外す
   end
 
   def destroy
     @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
+    @post = @like.post
     @like.destroy
-    redirect_to post_path(params[:post_id])
+    # redirect_to post_path(params[:post_id]),ajaxいらなければ外す
   end
 end
