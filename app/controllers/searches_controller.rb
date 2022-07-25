5c5b3329cedd6644847a58bea00a8aa95175926a
Ko-s-y <song.1209@icloud.com>
2022-07-25 17:38:25 +0900

    user検索機能実装

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -11,6 +11,8 @@ class SearchesController < ApplicationController
       @posts = Post.looks(params[:search], params[:word]).page(params[:page])
     elsif @range == "Comment"
       @comments = Comment.looks(params[:search], params[:word]).page(params[:page])
+    elsif @range == "User"
+      @users = User.looks(params[:search], params[:word]).page(params[:page])
     else
       flash[:alert] = "不正な値です。キーワードを正確に入力してください。"
       redirect_to posts_path
