b848c48b3ff99f5a696d9d2c1c2f4c7ad1089989
Ko-s-y <song.1209@icloud.com>
2022-07-26 20:25:26 +0900

    ゲストユーザーとしてログイン後timeline(posts/index)にリダイレクトするように変更

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,7 @@ class Users::SessionsController < Devise::SessionsController
   def new_guest
     user = User.guest
     sign_in user
-    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+    flash[:notice] = "ゲストユーザーとしてログインしました。"
+    redirect_to posts_path
   end
 end
