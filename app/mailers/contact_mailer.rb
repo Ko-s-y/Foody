c863afb9720257f19e7a72060622964bcc9f4c54
Ko-s-y <song.1209@icloud.com>
2022-07-30 00:53:27 +0900

    お問合せのbccを捨てアドからheroku環境変数のアドに変更

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,15 +1,9 @@
 class ContactMailer < ApplicationMailer
-  # ENV["FOODY_ADMIN_ADDRESS"]は本番環境で使用しているadminのheroku環境変数のicloudアドレス
-  # ENV["FOODY_PRODUCTION_USER"]は本番環境で使用しているadminのheroku環境変数のgmailアドレス
-  # ENV["USER_ADDRESS"]は開発環境用の.env環境変数のicloudアドレス
-  # ENV["ACTION_MAILER_USER"]は開発環境用の.env環境変数のgmailアドレス
-
   def contact_mail(contact, user)
     @contact = contact
     mail from: '"Foody運営" <noreply@foody.com>',
          to: user.email,
          bcc: ENV["FOODY_PRODUCTION_USER"],
-         bcc: ENV["ACTION_MAILER_USER"],
          subject: "お問い合わせについて【自動送信】"
   end
 end
