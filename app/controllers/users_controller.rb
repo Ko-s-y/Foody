de269964ca24f1a650ccd4d83100ea89f6a68854
Ko-s-y <song.1209@icloud.com>
2022-07-29 23:49:59 +0900

    profileページでもreceived_counterメソッドを使いたかったので引数を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,13 @@ class UsersController < ApplicationController
   def show
     @posts = current_user.posts.all.order(created_at: :desc)
     @remember_posts = current_user.checked_remember_posts.order(created_at: :desc)
-    received_counter(@posts)
+    received_counter(current_user, @posts)
   end
 
   def profile
     @user = User.find_by(name: params[:name])
     @posts = @user.posts.all.order(created_at: :desc)
-    received_counter(@posts)
+    received_counter(@user, @posts)
   end
 
   def update
@@ -28,12 +28,12 @@ class UsersController < ApplicationController
     params.require(:user).permit(:avatar)
   end
 
-  def received_counter(posts)
+  def received_counter(user, posts)
     # いいねをもらった回数
     @received_like = 0
     @posts.each do |post|
       post.likes.each do |like|
-        if like.user_id != current_user.id
+        if like.user_id != user.id
           @received_like += 1
         end
       end
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
     @received_remember = 0
     @posts.each do |post|
       post.remembers.each do |remember|
-        if remember.user_id != current_user.id
+        if remember.user_id != user.id
           @received_remember += 1
         end
       end
