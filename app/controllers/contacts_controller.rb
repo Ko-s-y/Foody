5e9b262aeb95b92122c8e2961fe640a32086ec7d
Ko-s-y <song.1209@icloud.com>
2022-08-02 19:53:48 +0900

    contactsモデルのuserを紐づけるためカラム追加

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -24,6 +24,6 @@ class ContactsController < ApplicationController
   private
 
   def contact_params
-    params.require(:contact).permit(:name, :content)
+    params.require(:contact).permit(:user_id, :name, :content)
   end
 end
