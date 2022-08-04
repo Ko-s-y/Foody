74c78a11e887adbaba660e8fbbbe1250f2b4dee6
Ko-s-y <song.1209@icloud.com>
2022-08-04 14:23:50 +0900

    posts/editでも残文字数カウント機能追加

--- app/javascript/packs/post/edit.js
+++ app/javascript/packs/post/edit.js
@@ -22,3 +22,23 @@ $(function() {
     }
   });
 });
+
+// フォームの残文字数カウント関数
+$(function() {
+  let nowStrCount = $(".form-text").text().replace(/\n/g, "改行").length; //現在入力されている文字数, 改行は2文字扱い(Rails側に合わせて)
+  let restStrCount = 200 - nowStrCount; //残りの入力可能文字数
+
+  $(".form-str-counter").text( "残り" + restStrCount + "文字");
+  $(".form-text").on("keyup", function() {
+    let nowStrCount = $(this).val().replace(/\n/g, "改行").length; //formのvalue文字数を数える
+    let restStrCount = 200 - nowStrCount;
+
+    // 色の条件分岐, 残り　0文字以降赤で表示
+    if (nowStrCount > 199) {
+      $(".form-str-counter").css("color","red");
+    } else {
+      $(".form-str-counter").css("color","black");
+    }
+    $(".form-str-counter").text( "残り" + restStrCount + "文字");
+  });
+});
