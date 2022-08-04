635b68e7c3d1a56438946e7e288f29980c75e5f1
Ko-s-y <song.1209@icloud.com>
2022-08-04 14:33:26 +0900

    jsファイルコメント修正

--- app/javascript/packs/post/edit.js
+++ app/javascript/packs/post/edit.js
@@ -6,18 +6,18 @@ $(function() {
     $('.state').text(fileName.name);
 
     // 選択した画像ファイルのプレビュー機能
-    let file = this.files[0];  // .file_filedからデータを取得して変数fileに代入
-    let reader = new FileReader();  // FileReaderオブジェクトを作成
+    let file = this.files[0];  // .file_filedからデータを取得して変数fileに
+    let reader = new FileReader();  // FileReaderオブジェクトを作成し変数readerに
     reader.readAsDataURL(file);  // DataURIScheme文字列を取得
-    reader.onload = function() {  // 読み込みが完了したら処理が実行
+    reader.onload = function() {
       let image = this.result;  // 読み込んだファイルの内容を取得して変数imageに
       if ($('.current-post-image').length == 0) {  // 投稿に画像がなければ
-        $('.not-have-post-image').css({  // not-have-post-imageのスペース確保
-          'display': 'block' // defaultがnoneの為
+        $('.not-have-post-image').css({  // not-have-post-imageの表示領域確保
+          'display': 'block'
         });
         $('.not-have-post-image').attr({ src: image });
         } else {
-        $('.current-post-image').attr({ src: image });  // 投稿の画像がプレビューされていれば(持っていれば)画像データのみを入れ替え
+        $('.current-post-image').attr({ src: image });  // postが画像を持っていれば画像データのみを入れ替え
       }
     }
   });
