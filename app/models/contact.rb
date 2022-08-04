7164fcc29a30563bcc144147a17dba7353f4702b
Ko-s-y <song.1209@icloud.com>
2022-08-04 12:48:45 +0900

    お問合せ内容の文字数上限を300に指定

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,5 +2,5 @@ class Contact < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true, length: { maximum: 20 }
-  validates :content, presence: true
+  validates :content, presence: true, length: { maximum: 300 }
 end
