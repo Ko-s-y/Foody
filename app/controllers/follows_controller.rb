56820aea083672f9234ab557721a86180b1c179c
Ko-s-y <song.1209@icloud.com>
2022-08-08 14:13:23 +0900

    follow_controllerのリファクタリング

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,14 +1,18 @@
 class FollowsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :set_user, only: [:create, :destroy]
 
   def create
-    current_user.follow!(params[:user_id])
-    @user = User.find(params[:user_id])
+    current_user.follow!(@user.id)
     @user.create_notification_follow!(current_user)
   end
 
   def destroy
-    current_user.unfollow!(params[:user_id])
+    current_user.unfollow!(@user.id)
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:user_id])
   end
 end
