0c3dae1f1d361d17e76faf37daa25831525fcca2
Ko-s-y <song.1209@icloud.com>
2022-08-12 22:23:38 +0900

    userテーブルにconfirm関連のカラム追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
   devise :validatable, password_length: 8..128
 
   with_options presence: true,
