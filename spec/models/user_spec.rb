c536c806d4813c5c4fc80e56467afb36f67526e4
Ko-s-y <song.1209@icloud.com>
2022-08-20 03:29:36 +0900

    userモデルのvalidationテスト追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,27 +10,45 @@ RSpec.describe User, type: :model do
       expect(@user.valid?).to eq(true)
     end
 
-    it 'nameが空だとNG' do
+    it 'nameが空の場合は無効な状態であること' do
       @user.name = ''
       expect(@user.valid?).to eq(false)
     end
 
-    it 'emailが空だとNG' do
+    it 'nameが5文字以下の場合は無効な状態であること' do
+      @user = FactoryBot.build(:user, name: 'aaaaa')
+      @user.valid?
+      expect(@user.errors[:name]).to include('は6文字以上に設定して下さい。')
+    end
+
+    it 'nameが12文字以上の場合は無効な状態であること' do
+      @user = FactoryBot.build(:user, name: 'a' * 13)
+      @user.valid?
+      expect(@user.errors[:name]).to include('は12文字以下に設定して下さい。')
+    end
+
+    it 'emailが空の場合は無効な状態であること' do
       @user.email = ''
       expect(@user.valid?).to eq(false)
     end
 
-    it 'introductionが空だとNG' do
+    it 'introductionが空の場合は無効な状態であること' do
       @user.introduction = ''
       expect(@user.valid?).to eq(false)
     end
 
-    it 'passwordが空だとNG' do
+    it 'passwordが空の場合は無効な状態であること' do
       @user.password = ''
       expect(@user.valid?).to eq(false)
     end
 
-    it 'agreement_termsがfalseだとNG' do
+    it 'passwordが7文字以下の場合は無効な状態であること' do
+      @user = FactoryBot.build(:user, password: 'a' * 7)
+      @user.valid?
+      expect(@user.errors[:password]).to include('は8文字以上に設定して下さい。')
+    end
+
+    it 'agreement_termsが空の場合は無効な状態であること' do
       @user.agreement_terms = false
       expect(@user.valid?).to eq(false)
     end
