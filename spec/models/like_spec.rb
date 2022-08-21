8e4a6d09e33645743de0dc1fd63d494cef5aee02
Ko-s-y <song.1209@icloud.com>
2022-08-21 23:50:36 +0900

    like,rememberモデルテスト追加

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
-# RSpec.describe Like, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Like, type: :model do
+  let(:user) { FactoryBot.create(:user) }
+  let(:post) { FactoryBot.create(:post) }
+
+  before do
+    @like = build(:like)
+  end
+
+  it 'userと1対多となっている事' do
+    expect(Like.reflect_on_association(:user).macro).to eq :belongs_to
+  end
+
+  it 'postと1対多となっている事' do
+    expect(Like.reflect_on_association(:post).macro).to eq :belongs_to
+  end
+
+  it 'user,postが設定されていればlike出来る事' do
+    expect(@like).to be_valid
+  end
+
+  it "user_idが同じでもpost_idが違えばlikeできる" do
+    another_like = FactoryBot.create(:like)
+    expect(FactoryBot.create(:like, post_id: another_like.post_id)).to be_valid
+  end
+
+  it "post_idが同じでもuser_idが違えばlikeできる" do
+    another_like = FactoryBot.create(:like)
+    expect(FactoryBot.create(:like, user_id: another_like.user_id)).to be_valid
+  end
+end
--- spec/models/remember_spec.rb
+++ spec/models/remember_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
-# RSpec.describe Remember, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Remember, type: :model do
+  let(:user) { FactoryBot.create(:user) }
+  let(:post) { FactoryBot.create(:post) }
+
+  before do
+    @remember = build(:remember)
+  end
+
+  it 'userと1対多となっている事' do
+    expect(Remember.reflect_on_association(:user).macro).to eq :belongs_to
+  end
+
+  it 'postと1対多となっている事' do
+    expect(Remember.reflect_on_association(:post).macro).to eq :belongs_to
+  end
+
+  it 'user,postが設定されていればremember出来る事' do
+    expect(@remember).to be_valid
+  end
+
+  it "user_idが同じでもpost_idが違えばrememberできる" do
+    another_remember = FactoryBot.create(:remember)
+    expect(FactoryBot.create(:remember, post_id: another_remember.post_id)).to be_valid
+  end
+
+  it "post_idが同じでもuser_idが違えばrememberできる" do
+    another_remember = FactoryBot.create(:remember)
+    expect(FactoryBot.create(:remember, user_id: another_remember.user_id)).to be_valid
+  end
+end
