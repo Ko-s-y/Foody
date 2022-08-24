0202ae60c523a476c4d42177d11d9e605d3686c2
Ko-s-y <song.1209@icloud.com>
2022-08-24 15:04:30 +0900

    notificationモデルのrequest_spec追加

--- spec/requests/notifications_spec.rb
+++ spec/requests/notifications_spec.rb
@@ -1,7 +1,51 @@
 require 'rails_helper'
 
-# RSpec.describe "Notifications", type: :request do
-#   describe "GET /index" do
-#     pending "add some examples (or delete) #{__FILE__}"
-#   end
-# end
+RSpec.describe "Notifications", type: :request do
+  before do
+    @user = FactoryBot.create(:user)
+  end
+
+  describe '#index' do
+    context '認証済みユーザーの場合' do
+      it 'リクエストが成功する事' do
+        sign_in @user
+        get notifications_path
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'ゲストの場合' do
+      it 'リクエストが失敗する事' do
+        get notifications_path
+        expect(response.status).to eq 302
+      end
+
+      it 'ログインページにリダイレクトする事' do
+        get notifications_path
+        expect(response).to redirect_to user_session_path
+      end
+    end
+  end
+
+  describe '#checked' do
+    context '認証済みユーザーの場合' do
+      it 'リクエストが成功する事' do
+        sign_in @user
+        get checked_notifications_path
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'ゲストの場合' do
+      it 'リクエストが失敗する事' do
+        get notifications_path
+        expect(response.status).to eq 302
+      end
+
+      it 'ログインページにリダイレクトする事' do
+        get notifications_path
+        expect(response).to redirect_to user_session_path
+      end
+    end
+  end
+end
--- spec/requests/searches_spec.rb
+++ spec/requests/searches_spec.rb
@@ -13,17 +13,17 @@ RSpec.describe "Searches", type: :request do
         expect(response).to redirect_to posts_path
       end
     end
-  end
 
-  context 'ゲストの場合' do
-    it 'リクエストが失敗する事' do
-      get searches_result_path
-      expect(response.status).to eq 302
-    end
+    context 'ゲストの場合' do
+      it 'リクエストが失敗する事' do
+        get searches_result_path
+        expect(response.status).to eq 302
+      end
 
-    it 'ログインページにリダイレクトする事' do
-      get searches_result_path
-      expect(response).to redirect_to user_session_path
+      it 'ログインページにリダイレクトする事' do
+        get searches_result_path
+        expect(response).to redirect_to user_session_path
+      end
     end
   end
 end
