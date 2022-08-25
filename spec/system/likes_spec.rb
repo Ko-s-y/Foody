f3b5dd76889fa1ce3d4f6f0a56e519fa1aab8dee
Ko-s-y <song.1209@icloud.com>
2022-08-25 18:00:42 +0900

    likeモデルにテスト追加

--- spec/system/likes_spec.rb
+++ spec/system/likes_spec.rb
@@ -2,25 +2,37 @@ require 'rails_helper'
 
 RSpec.describe "Likes", type: :system, js: true do
   before do
-    @active_like_user = FactoryBot.create(:user)
-    @passive_like_user = FactoryBot.create(:user)
-    @post = FactoryBot.create(:post, user_id: @passive_like_user.id)
+    @me = FactoryBot.create(:user)
+    @other_user = FactoryBot.create(:user)
+    @my_post = FactoryBot.create(:post, user_id: @me.id)
+    @other_user_post = FactoryBot.create(:post, user_id: @other_user.id)
   end
 
   describe '投稿に対する良いね機能について' do
     before do
-      login_as(@active_like_user)
-      visit post_path(@post.id)
+      login_as(@me)
     end
 
-    it '投稿を良いね、良いね解除できる事' do
+    it '自分の投稿を良いね、良いね解除できる事' do
+      visit post_path(@my_post.id)
       find('#like-btn').click
       expect(page).to have_selector '#unlike-btn'
-      expect(@post.likes.count).to eq(1)
+      expect(@my_post.likes.count).to eq(1)
 
       find('#unlike-btn').click
       expect(page).to have_selector '#like-btn'
-      expect(@post.likes.count).to eq(0)
+      expect(@my_post.likes.count).to eq(0)
+    end
+
+    it '他人の投稿を良いね、良いね解除できる事' do
+      visit post_path(@other_user_post.id)
+      find('#like-btn').click
+      expect(page).to have_selector '#unlike-btn'
+      expect(@other_user_post.likes.count).to eq(1)
+
+      find('#unlike-btn').click
+      expect(page).to have_selector '#like-btn'
+      expect(@other_user_post.likes.count).to eq(0)
     end
   end
 end
