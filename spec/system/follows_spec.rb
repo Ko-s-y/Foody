00a71e1bedeeb11073038be8c1d2c6eb5b682e04
Ko-s-y <song.1209@icloud.com>
2022-08-25 17:24:59 +0900

    followモデルのsystem_spec記述訂正

--- spec/system/follows_spec.rb
+++ spec/system/follows_spec.rb
@@ -1,17 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "Relationships", type: :system, js: true do
+RSpec.describe "Follows", type: :system, js: true do
   before do
     @passive_follow_user = FactoryBot.create(:user)
-    @active_follow_user = FactoryBot.create(:user, name: "followtest")
+    @active_follow_user = FactoryBot.create(:user)
   end
 
-  describe '#create,#destroy' do
-    it 'ユーザーをフォロー、フォロー解除できる' do
+  describe 'userフォロー機能について' do
+    before do
       login_as(@passive_follow_user)
-      visit users_profile_path("followtest") # user名がパスに必要な為
-      find('#follow-btn').click
+      visit users_profile_path(@active_follow_user.name) # user名がパスに必要な為
+    end
 
+    it 'ユーザーをフォロー、フォロー解除できる事' do
+      find('#follow-btn').click
       expect(page).to have_content 'フォロー中'
       expect(@active_follow_user.followed.count).to eq(1)
       expect(@passive_follow_user.follower.count).to eq(1)
